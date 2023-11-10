38275474fefc6b0524722b3473bd9e64bfa6139a
Lucas Bomfim <lucasbomfimsobral@gmail.com>
2023-11-10 10:11:39 -0300

    commit inicial

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,15 +2,11 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("form-sorteador").addEventListener("submit", function(evento) {
         evento.preventDefault();
         let numeroMaximo = document.getElementById("numero-maximo").value;
-        // transformar STRING em NUMBER
         numeroMaximo = parseInt(numeroMaximo);
-        // Math.random = Gerar número aleatório
+
         let numeroAleatorio = Math.random() * numeroMaximo;
-        // Math.ceil = Arredondar para cima
-        // Math.floor = Arredondar para baixo
-        // Math.round = Arredondar para cima acima de .50 e para baixo abaixo de .50
         numeroAleatorio = Math.floor(numeroAleatorio + 1);
-        // innerText = criar texto no html
+        
         document.getElementById("resultado-valor").innerText = numeroAleatorio;
         document.querySelector(".resultado").style.display = "block";
     })
