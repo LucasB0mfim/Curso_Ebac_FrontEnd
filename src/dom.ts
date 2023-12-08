4c0f7f379df44de19f8fe08aa67153359c33f018
LucasB0mfim <143045095+LucasB0mfim@users.noreply.github.com>
2023-12-08 11:29:57 -0300

    Delete src/dom.ts

--- src/dom.ts
+++
@@ -1,7 +0,0 @@
-const campoNome  = document.getElementById('nome');
-const formulario = document.getElementById('form');
-const botaoEnviar = document.getElementById('btn-enviar');
-
-botaoEnviar?.addEventListener('click', (e: MouseEvent) => {
-    
-})
